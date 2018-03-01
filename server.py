7aa4abc90daf21caa0dddcc4dfdaab77279a3c1b
Alex Rodriguez <thuperflee@gmail.com>
2018-03-01 02:14:19 -0800

    Rewrote backend for scalability

--- server.py
+++ server.py
@@ -4,10 +4,10 @@ import time
 import os
 
 
-event = "lastchance"
+current_tournament_id = 2912
 
 clean = str.maketrans('', '', """ ^$#@~`&;:|{()}[]<>+=!?.,\/*-_"'""")
-sanitize = str.maketrans('', '', """^$#@~`;:|{()}[]+=\*_"'""")
+sanitize = str.maketrans('', '', """^~`;:|{()}[]+=\*_"'""")
 
 #Configuration
 db_user = os.environ['DB_USER']
@@ -20,26 +20,23 @@ db = pymysql.connect(db_ip, db_user, db_password, db_name)
 c = db.cursor()
 
 
-def report(team_name, auton_score, driver_score, scouter_id, highest_stack, notes=''):
-    # check to see if team is valid
-    #c.execute("SELECT team_name FROM reports WHERE team_name='" + team_name + "'")
-    #if len(c.fetchall()):
-    # submit report
-    date_time = int(time.time())
-    c.execute('INSERT INTO '+event+'(team_name, auton_score, driver_score, highest_stack, notes, date_time, scouter_id) VALUES ("'+team_name+'",'+str(auton_score)+','+str(driver_score)+','+str(highest_stack)+',"'+notes+'",'+str(date_time)+','+str(scouter_id)+")")
+def report(team_name, auton_score, driver_score, highest_stack, reporter_ip, notes=""):
+    time_stamp = int(time.time())
+    c.execute('INSERT INTO Reports(team_name, auton_score, driver_score, highest_stack, notes, time_stamp, reporter_ip, tournament_id)' +
+              'VALUES ("'+team_name+'",'+str(auton_score)+','+str(driver_score)+','+str(highest_stack)+',"'+notes+'",'+str(time_stamp)+','+str(reporter_ip)+','+str(current_tournament_id)+")")
     db.commit()
 
-def pull_reports(team_name=None):
+def pull_reports(tournament_id, team_name=None):
     if team_name:
-        c.execute("SELECT * FROM "+event+" WHERE team_name='" + team_name + "'")
+        c.execute('SELECT * FROM Reports WHERE team_name="' + team_name + '" and tournament_id=' + str(tournament_id))
     else:
-        c.execute("SELECT * FROM "+event)
+        c.execute('SELECT * FROM Reports WHERE tournament_id=' + str(tournament_id))
     return c.fetchall()
 
-def get_unscouted_robots(): # Returns a list of unscouted robots in string name form
+def get_unscouted_robots(tournament_id): # Returns a list of unscouted robots in string name form
 	scouted = []
 	unscouted = []
-	for r in pull_reports():
+	for r in pull_reports(tournament_id):
 		for t in valid_teams:
 			if t == r[0] and t not in scouted:
 				scouted.append(t)
@@ -48,8 +45,8 @@ def get_unscouted_robots(): # Returns a list of unscouted robots in string name 
 			unscouted.append(r)
 	return unscouted
 
-def compress_reports(): # Compile data and find average values for each robot
-    c.execute("SELECT * FROM "+event)
+def compress_reports(tournament_id): # Compile data and find average values for each robot
+    c.execute('SELECT team_name, auton_score, driver_score, highest_stack, notes FROM Reports WHERE tournament_id=' + str(tournament_id))
     robotData = c.fetchall()
     robots = []
     for row in robotData:  # Find unique robots
@@ -80,28 +77,26 @@ def compress_reports(): # Compile data and find average values for each robot
     return compressedData
 
 
-def robot_power(robot_stats): # Enter compiled data row as input. Formula = bestdriver + avgdriver + bestauton + avgauton + higheststack
-	
+def robot_power(robot_stats): # Enter compiled data row as input. Formula: power = best_driver + avg_driver + best_auton + avg_auton + highest_stack * 2 + times_scouted
 	return robot_stats[1] + robot_stats[2] + robot_stats[3] + robot_stats[4] + robot_stats[5] * 2 + robot_stats[7]
 
 
-def reverse_bubble_sort(collection): # Sort reports by best driver score
+def reverse_bubble_sort(collection): # Sort reports by best robot power
 	length = len(collection)
-	for i in range(length-1, -1, -1):#range(length-1, -1, -1)
-		for j in range(i):#range(1, i)
+	for i in range(length-1, -1, -1):
+		for j in range(i):
 			if robot_power(collection[j]) > robot_power(collection[j+1]):
 				collection[j], collection[j+1] = collection[j+1], collection[j]
 	return collection[::-1]
-
-def retrieve_data(table):
-	pass
 
 
 app = Flask(__name__)
 
 @app.route('/') # Home page
 def index():
-	return render_template('index.html', status="Seaquam Robotics Scouting")
+	c.execute('SELECT tournament_name FROM Tournaments WHERE tournament_id=' + str(current_tournament_id))
+	tournament_name = c.fetchall()[0][0]
+	return render_template('index.html', current_tournament_name=tournament_name, current_tournament_id=current_tournament_id, status="Seaquam Robotics Scouting")
 
 @app.route('/scouting', methods=['POST', 'GET']) # Scouting submission page
 def scouting():
@@ -110,8 +105,7 @@ def scouting():
 		auton_score = 0
 		score = 0
 		team_name = request.form['team'].translate(clean).upper()
-		if 1==1:
-		#if team_name in valid_teams:
+		if team_name in valid_teams:
 			print("Report submitted for " + team_name)
 			#Autonomous
 			if request.form['auton_mobile_goal'] == "far":
@@ -123,7 +117,7 @@ def scouting():
 			auton_score += int(request.form['auton_cones_stacked']) * 2
 			
 			if int(request.form['driver_num_mobile_mid']) + int(request.form['driver_num_mobile_near']) + (request.form.get('driver_is_mobile_far') == "yes") > 4:
-				return render_template('scouting_sent.html', status=Markup('<span class="error">Error:</span> Too many mobile goals submitted. Only 4 exist.'))
+				return render_template('index.html', status=Markup('<span class="error">Error:</span> Too many mobile goals submitted. Only 4 exist.'))
 
 			#Driver/Total
 			if request.form.get('driver_is_mobile_far') == "yes":
@@ -133,29 +127,32 @@ def scouting():
 			score += int(request.form['driver_num_cones_tower']) * 2
 
 			highest_stack = request.form['driver_highest_stack']
-			scouter_id = request.environ['REMOTE_ADDR'].translate(clean)
+			reporter_ip = request.environ['REMOTE_ADDR'].translate(clean)
 
-			report(team_name, auton_score, score, scouter_id, highest_stack, request.form['notes'].translate(sanitize))
+			#team_name, auton_score, driver_score, highest_stack, reporter_ip, notes=""
+
+			report(team_name, auton_score, score, highest_stack, reporter_ip, request.form['notes'].translate(sanitize))
 		else:
 			if len(team_name) == 0:
 				team_name = "NULL"
-			return render_template('scouting_sent.html', status=Markup('<span class="error">Error:</span> Invalid team name: ' + team_name + " not found in list of participating robots."))
+			return render_template('index.html', status=Markup('<span class="error">Error:</span> Invalid team name: ' + team_name + " not found in list of participating robots."))
 		return render_template('index.html', status="Scouting report sent successfully")
 	elif request.method == 'GET':
 		return render_template('scouting.html', error=error)
-	return "Error: Method type not GET or POST" #Literally impossible
 
-@app.route('/data') # Compiled scouting reports page
-def data():
+@app.route('/data/<int:tournament_id>') # Compiled scouting reports page
+def data(tournament_id):
+	c.execute('SELECT tournament_name FROM Tournaments WHERE tournament_id=' + str(tournament_id))
+	tournament_name = c.fetchall()[0][0]
 	robots_data = ''
-	for i, row in enumerate(reverse_bubble_sort(compress_reports())):
+	for i, row in enumerate(reverse_bubble_sort(compress_reports(tournament_id))):
 		robots_data += '<tr><td>'+str(i+1)+'</td>'
 		for cell in row:
 			robots_data += '<td>'+str(cell)+'</td>'
 		robots_data +='</tr>'
 	robots_data_html = Markup(robots_data)
 
-	unscouted_robots = get_unscouted_robots()
+	unscouted_robots = get_unscouted_robots(tournament_id)
 	unscouted = ''
 	if unscouted_robots:
 		unscouted += '<h2>Not Yet Scouted:</h2><div class="unscouted">'
@@ -165,58 +162,39 @@ def data():
 		unscouted += r+' '
 	unscouted_html = Markup(unscouted+'</div>')
 
-	return render_template('data.html', data=robots_data_html, unscouted=unscouted_html)
+	return render_template('data.html', tournament_name=tournament_name, data=robots_data_html, unscouted=unscouted_html)
 
-@app.route('/old_data')
-def old_data():
-	return render_template('old_data.html')
-
-
-@app.route('/comox')
-def comox():
-	return render_template('comox.html')
-
-@app.route('/mcmath')
-def mcmath():
-	return render_template('mcmath.html')
+@app.route('/past-tournaments')
+def past_tournaments():
+	tournaments_html = ''
+	c.execute('SELECT tournament_id, tournament_name, team_list FROM Tournaments')
+	for t in c.fetchall():
+		tournaments_html += '<a class="box2" href="data/' + str(t[0]) + '">' + t[1] + '</a>'
+	tournaments_html = Markup(tournaments_html)
+	return render_template('past_tournaments.html', tournaments=tournaments_html)
 
 @app.route('/agenda')
 def agenda():
 	return render_template('agenda.html')
-
 
 @app.errorhandler(404) #Error 404 page
 def page_not_found(e):
 	return render_template('404.html'), 404
 
 if __name__ == '__main__':
-	c.execute('CREATE TABLE IF NOT EXISTS '+event+'(team_name TEXT, auton_score INT, driver_score INT, highest_stack INT, notes TEXT, date_time BIGINT, scouter_id BIGINT)')
-	#TODO Swap out hard-coding for scraped data stored in a database https://www.robotevents.com/robot-competitions/vex-robotics-competition/RE-VRC-17-2911.html 
-	valid_teams = ['2A','2C','2E','2G','2H','2N','2X','2Y',
-                   '600U','600V',
-                   '1010E','1010F','1010G','1010H','1010M','1010R','1010V','1010W','1010Y','1011X',
-                   '1136C',
-                   '1346A','1346B','1346C',
-                   '1521A','1521B',
-                   '1700B',
-                   '4549A','4549C',
-                   '5123A',
-                   '6264A','6264C','6264D',
-                   '7842C','7842E','7842G','7842H','7842J','7842K','7842N',
-                   '8001A',
-                   '9181A','9181C','9181D',
-                   '9594C','9594M','9594R',
-                   '10445A','10445B',
-                   '24469A',
-                   '57421A',
-                   '64008G',
-                   '77174A',
-                   '83720A',
-                   '98549A','98549Y'
-                   #,'TEST'
-                   ] #Remove 'TEST' from list during real use
+
+	# Create tables if they do not already exist
+	c.execute('SHOW TABLES')
+	tbls = str(c.fetchall())
+	if 'Tournaments' not in tbls:
+		c.execute('CREATE TABLE IF NOT EXISTS Tournaments(tournament_id INT, tournament_name TEXT, team_list TEXT, PRIMARY KEY (tournament_id))')
+	if 'Reports' not in tbls:
+		c.execute('CREATE TABLE IF NOT EXISTS Reports(team_name TEXT, auton_score INT, driver_score INT, highest_stack INT, notes TEXT, time_stamp BIGINT, reporter_ip BIGINT, tournament_id INT)')
+	
+	c.execute('SELECT team_list FROM Tournaments WHERE tournament_id=' + str(current_tournament_id))
+	valid_teams = c.fetchall()
+	valid_teams = valid_teams[0][0].split()
 
 	app.run(threaded=True, debug=True, host='0.0.0.0', port=8000)
 
 db.close()
-print('done')
